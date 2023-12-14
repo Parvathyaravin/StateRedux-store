da0f032791a1e410d6ef05b6f54a35ac9cee69b0
Parvathyaravin <124186372+Parvathyaravin@users.noreply.github.com>
2023-12-14 06:53:29 -0700

    Add files via upload

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  // TODO: Set up combine reducers
+  // https://laurenyz.medium.com/redux-with-hooks-basic-setup-6c1ea756d4cc -> step 7 to continue...
+    // Set up new functions that live above root reducer to set up initial state of each component and any subsequent components needed from a particular page
+
+  // Use reducers to create a more manageable stack of API calls
+
+  // Find out what needs to be changed and where Reacts context APIs live
+    
+});
+
+export default rootReducer;
